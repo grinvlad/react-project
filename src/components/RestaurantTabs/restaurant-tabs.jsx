6c798ef8357dfb37c64f2a77e9b7f33b3f6f7c19
Vlad Grin <vortexnz21@gmail.com>
2025-03-17 16:51:55 +0300

    redundant RestaurantTab component removed

--- src/components/RestaurantTabs/restaurant-tabs.jsx
+++ src/components/RestaurantTabs/restaurant-tabs.jsx
@@ -1,21 +1,14 @@
-function RestaurantTab({ isActiveTab, restaurantName, onClickedTab }) {
-  return (
-    <button disabled={isActiveTab} onClick={onClickedTab}>
-      {restaurantName}
-    </button>
-  );
-}
-
 export function RestaurantTabs({ restaurants, clickedTab, handleClickedTab }) {
   return (
     <ul className="restaurant-tabs">
       {restaurants.map(({ id, name }) => (
         <li key={id}>
-          <RestaurantTab
-            isActiveTab={id === clickedTab ? true : false}
-            restaurantName={name}
-            onClickedTab={handleClickedTab(id)}
-          />
+          <button
+            disabled={id === clickedTab ? true : false}
+            onClick={handleClickedTab(id)}
+          >
+            {name}
+          </button>
         </li>
       ))}
     </ul>
