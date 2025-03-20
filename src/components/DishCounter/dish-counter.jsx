fe9508f4f01550a2d1e81af1db2a00eca74fa853
Vlad Grin <vortexnz21@gmail.com>
2025-03-20 14:57:57 +0300

    Took out the logic from DishCounter into custom hook

--- src/components/DishCounter/dish-counter.jsx
+++ src/components/DishCounter/dish-counter.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
+import { useDishCounter } from "./use-dish-counter";
 import { Counter } from "../Counter/counter";
 
 export function DishCounter({ min = 0, max = 5 }) {
-  const [counter, setCounter] = useState(0);
-
-  const increment = () => setCounter(Math.min(counter + 1, max));
-  const decrement = () => setCounter(Math.max(counter - 1, min));
+  const { counter, increment, decrement } = useDishCounter(min, max);
 
   return (
     <Counter value={counter} onIncrement={increment} onDecrement={decrement} />
