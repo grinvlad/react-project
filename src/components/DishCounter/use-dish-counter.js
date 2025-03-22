f5b5b12949bbbfdc838f3fbf75ddd545fc764ccd
Vlad Grin <vortexnz21@gmail.com>
2025-03-22 14:06:29 +0300

    Wrapped *more* 'handle' functions with useCallback

--- src/components/DishCounter/use-dish-counter.js
+++ src/components/DishCounter/use-dish-counter.js
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export function useDishCounter(min = 0, max = 5) {
   const [counter, setCounter] = useState(min);
 
-  const increment = () => setCounter(Math.min(counter + 1, max));
-  const decrement = () => setCounter(Math.max(counter - 1, min));
+  const increment = useCallback(
+    () => setCounter((counter) => Math.min(counter + 1, max)),
+    [max]
+  );
+  const decrement = useCallback(
+    () => setCounter((counter) => Math.max(counter - 1, min)),
+    [min]
+  );
 
   return {
     counter,
